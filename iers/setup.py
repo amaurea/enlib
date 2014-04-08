94e5104007599d8562fc05e45ba8c5cc73feb0ca
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2014-04-08 12:38:31 -0400

    Fixed unnecessary dependance of iers.

--- iers/setup.py
+++ iers/setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 setup(name = "iers",
-		ext_modules=[Extension("_iers", sources=["iers.c","iers_wrap.c"], libraries=["iers"])],
+		ext_modules=[Extension("_iers", sources=["iers.c","iers_wrap.c"])],
 		py_modules=["iers"])
