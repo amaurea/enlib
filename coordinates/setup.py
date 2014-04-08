f809fc2be7ad5e9b3967950b61c2a1952759b79a
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2014-04-08 13:28:08 -0400

    Added slalib in the coordinates directory. I usually don't include dependencies like this in the repository itself, but I use slalib a bit unconventionally, and it is somewhat hard to find the fortran version.

--- coordinates/setup.py
+++ coordinates/setup.py
@@ -1,3 +1,3 @@
 from numpy.distutils.core import setup, Extension
-wrapper = Extension('pyfsla', sources=['pyfsla.f90'], libraries=['sla'])
+wrapper = Extension('pyfsla', sources=['pyfsla.f90'], libraries=['sla'], library_dirs=['.'])
 setup(ext_modules = [wrapper])
