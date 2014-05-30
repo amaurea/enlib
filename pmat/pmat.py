4d64da49ee13b770d9ec484fb90a9f093b9b7f3f
Sigurd Næss <sigurdkn@gmail.com>
2014-05-30 15:24:03 +0100

    Added linear interpolation pointing matrix, though it's not properly tested yet. Made it configurable which one to use in Pmat constructor.

--- pmat/pmat.py
+++ pmat/pmat.py
@@ -5,18 +5,6 @@ Both of these are allowed to modify both arguments, though usually
 forward will update tod based on m, and backward will update m based on tod.
 The reason for allowing the other argument to be modified is to make it easier
 to incrementally project different parts of the signal.
-
-We need to be very clear about how we interpret pixel coordinates here.
-We assign the value of a pixel to its center point. In nearest neighbor
-interpolation this is also the hit-average inside that pixel. This means
-that the integer pixel index to assign to will be ipix = round(pix),
-not floor(pix). Does it?
-
-     0    1    2    3
-  +----+----+----+----+
-
-If the floating point pixel coordinates are relative to pixel
-centers, then we should use round. Otherwise it's still floor.
 """
 import numpy as np
 from enlib import enmap, interpol, utils, coordinates
@@ -26,25 +14,41 @@ class PointingMatrix:
 	def forward(self, tod, m): raise NotImplementedError
 	def backward(self, tod, m): raise NotImplementedError
 
-class pos2pix:
-	def __init__(self, scan, template, sys):
-		self.scan, self.template, self.sys = scan, template, sys
-	def __call__(self, ipos):
-		shape = ipos.shape[1:]
-		ipos  = ipos.reshape(ipos.shape[0],-1)
-		time  = self.scan.mjd0 + ipos[0]/utils.day2sec
-		opos = coordinates.transform(self.scan.sys, self.sys, ipos[1:], time=time, site=self.scan.site, pol=True)
-		opix = np.zeros((4,)+ipos.shape[1:])
-		opix[:2] = self.template.sky2pix(opos[1::-1])
-		opix[2]  = np.cos(2*opos[2])
-		opix[3]  = np.sin(2*opos[2])
-		return opix.reshape((opix.shape[0],)+shape)
+class MapPmat(PointingMatrix):
+	"""Fortran-accelerated scan <-> enmap pointing matrix implementation.
+	20 times faster than the slower python+numpy implementation below."""
+	def __init__(self, scan, template, sys="equ", order=0):
+		box = np.array(scan.box); box[1] += (box[1]-box[0])*1e-3 # margin to avoid rounding errors
+		ipol = interpol.build(pos2pix(scan,template,sys), interpol.ip_linear, box, [1e-3,1e-3,utils.arcsec,utils.arcsec])
+		self.rbox = ipol.box
+		self.nbox = np.array(ipol.ys.shape[4:])
+		# ipol.ys has shape [2t,2az,2el,{ra,dec,cos,sin},t,az,el]
+		# fortran expects [{ra,dec,cos,sin},{y,dy/dt,dy/daz,dy,del,...},pix]
+		# This format allows us to avoid hard-coding the number of input dimensions,
+		# and is forward compatible for higher order interpolation later.
+		# The disadvantage is that the ordering becomes awkard at higher order.
+		n = self.rbox.shape[1]
+		self.ys = np.asarray([ipol.ys[(0,)*n]] + [ipol.ys[(0,)*i+(1,)+(0,)*(n-i-1)] for i in range(n)])
+		self.ys = np.rollaxis(self.ys.reshape(self.ys.shape[:2]+(-1,)),-1).astype(np.float32)
+		self.comps= np.arange(template.shape[0])
+		self.scan  = scan
+		self.order = order
+		if order == 0:
+			self.func = pmat_core.pmat_nearest
+		elif order == 1:
+			self.func = pmat_core.pmat_linear
+		else:
+			raise NotImplementedError("order > 1 is not implemented")
+	def forward(self, tod, m):
+		self.func( 1, tod.T, m.T, self.scan.boresight.T, self.scan.offsets.T, self.scan.comps.T, self.comps, self.rbox.T, self.nbox, self.ys.T)
+	def backward(self, tod, m):
+		self.func(-1, tod.T, m.T, self.scan.boresight.T, self.scan.offsets.T, self.scan.comps.T, self.comps, self.rbox.T, self.nbox, self.ys.T)
 
-class MapPointingMatrixNearestSlow(PointingMatrix):
+class MapPmatSlow(PointingMatrix):
 	"""Reference implementation of the simple nearest neighbor
-	pointing matrix. Slow - not meant for serious use. It's interesting
+	pointing matrix. Very slow - not meant for serious use. It's interesting
 	as an example of how a reasonable python-only implementation would
-	perform."""
+	perform. Uses trilinear interpolation for coordinates."""
 	def __init__(self, scan, template, sys="equ"):
 		# Build interpolator between scan coordinates and
 		# template map coordinates.
@@ -61,46 +65,24 @@ class MapPointingMatrixNearestSlow(PointingMatrix):
 	def backward(self, tod, m):
 		for d in range(tod.shape[0]):
 			pix, phase = self.get_pix_phase(d)
-			print np.min(pix,1), np.max(pix,1)
 			for c in range(m.shape[0]):
 				m[c] += np.bincount(np.ravel_multi_index(pix[:2], m.shape[-2:]), tod[d], m[c].size).reshape(m[c].shape)
-			print np.min(m), np.max(m)
 	def forward(self, tod, m):
 		for d in range(tod.shape[0]):
-			print d
 			pix, phase = self.get_pix_phase(d)
 			tod[d] += np.sum(m[:,pix[0],pix[1]]*phase[:m.shape[0]],0)
 
-# To go faster, we need two sets of interpolation coefficients in fotran.
-# The first is the one used to compute the pixel coordinates of each sample.
-# This can be the linear coefficients from ip_linear. ip_linear itself is
-# relatively slow, but it only needs to be called when building the pointing
-# matrix, and takes a few sec. We then use these in a much faster fortran
-# implementation.
-#
-# The second level of interpolation is the pixel value interpolation. These
-# interpolation coefficients are too many to be precomputed. They must be
-# computed on the fly in fortran.
-
-class MapPointingMatrixNearestFast(PointingMatrix):
-	def __init__(self, scan, template, sys="equ"):
-		box = np.array(scan.box); box[1] += (box[1]-box[0])*1e-3 # margin to avoid rounding errors
-		ipol = interpol.build(pos2pix(scan,template,sys), interpol.ip_linear, box, [1e-3,1e-3,utils.arcsec,utils.arcsec])
-		self.rbox = ipol.box
-		self.nbox = np.array(ipol.ys.shape[4:])
-		# ipol.ys has shape [2t,2az,2el,{ra,dec,cos,sin},t,az,el]
-		# fortran expects [{ra,dec,cos,sin},2el,2az,2t,pix]
-		# so flatten into pix, and move pix first
-		# ys is flattened in C order.
-		# Since I will almost always want just y and grad y, let's order ys as
-		# [y,dy/dt,dy/daz,dy/del,...]. That way the lower derivatives don't need
-		# to worry about the presence of higher ones
-		n = self.rbox.shape[1]
-		self.ys = np.asarray([ipol.ys[(0,)*n]] + [ipol.ys[(0,)*i+(1,)+(0,)*(n-i-1)] for i in range(n)])
-		self.ys = np.rollaxis(self.ys.reshape(self.ys.shape[:2]+(-1,)),-1).astype(np.float32)
-		self.comps= np.arange(template.shape[0])
-		self.scan = scan
-	def forward(self, tod, m):
-		pmat_core.map_nearest( 1, tod.T, m.T, self.scan.boresight.T, self.scan.offsets.T, self.scan.comps.T, self.comps, self.rbox.T, self.nbox, self.ys.T)
-	def backward(self, tod, m):
-		pmat_core.map_nearest(-1, tod.T, m.T, self.scan.boresight.T, self.scan.offsets.T, self.scan.comps.T, self.comps, self.rbox.T, self.nbox, self.ys.T)
+class pos2pix:
+	"""Transforms from scan coordintaes to pixel-center coordinates."""
+	def __init__(self, scan, template, sys):
+		self.scan, self.template, self.sys = scan, template, sys
+	def __call__(self, ipos):
+		shape = ipos.shape[1:]
+		ipos  = ipos.reshape(ipos.shape[0],-1)
+		time  = self.scan.mjd0 + ipos[0]/utils.day2sec
+		opos = coordinates.transform(self.scan.sys, self.sys, ipos[1:], time=time, site=self.scan.site, pol=True)
+		opix = np.zeros((4,)+ipos.shape[1:])
+		opix[:2] = self.template.sky2pix(opos[1::-1])
+		opix[2]  = np.cos(2*opos[2])
+		opix[3]  = np.sin(2*opos[2])
+		return opix.reshape((opix.shape[0],)+shape)
--- pmat/pmat_core.f90
+++ pmat/pmat_core.f90
@@ -2,7 +2,7 @@ module pmat_core
 
 contains
 
-! Performance for tod[445,262000]:
+! Performance for tod[445,262000] on laptop with OMP_NUM_THREADS=4
 !  dp_all   bounds tri Ofast   : 3.6131 3.6265 3.6019
 !  dp_bore  bounds tri Ofast   : 3.8680 3.8867 3.8988
 !  sp_all   bounds tri Ofast   : 3.3886 3.3987 3.3839
@@ -14,8 +14,16 @@ contains
 !  sp_all   bounds tri O2      : 3.5449 3.5517 3.5351
 !  sp_all   bounds tri Os      : 4.1127 4.1443 4.1305
 !  sp_all   bounds tri Of omit : 3.3724 3.4091 3.3960
-! Bounds checking costs about 1%. That's worth it.
-subroutine map_nearest( &
+!  sp_all   bounds tri Of col1 : 3.3961 3.4003 3.4068
+!
+! Interpolation accuracy at 1e-3,1e-3,arcsec,arcsec
+!  std(tri-grd): 4.76968216E-06   2.86180921E-06   1.63026237E-09   9.31578481E-10
+! No point in using the 35% slower tridiagonal interpolation. In that case,
+! is there a point in using the clunky ys interface?
+!
+! nint is 5% slower than floor
+
+subroutine pmat_nearest( &
 		dir,                              &! Direction of the projection: 1: forward (map2tod), -1: backard (tod2map)
 		tod, map,                         &! Main inputs/outpus
 		bore, det_pos, det_comps,  comps, &! Input pointing
@@ -25,13 +33,12 @@ subroutine map_nearest( &
 	implicit none
 	! Parameters
 	integer(4), intent(in)    :: dir, comps(:), nbox(:)
-	real(4),    intent(in)    :: bore(:,:)
+	real(4),    intent(in)    :: bore(:,:), ys(:,:,:) ! comp, derivs, pix
 	real(4),    intent(in)    :: det_pos(:,:), det_comps(:,:), rbox(:,:)
-	real(4),    intent(in)    :: ys(:, :, :) ! comp, derivs, pix
 	real(4),    intent(inout) :: tod(:,:), map(:,:,:)
 	! Work
 	integer(4), parameter :: bz = 321
-	integer(4) :: ndet, nsamp, di, si, nproc, myid, ic, i, j, k, l, nj
+	integer(4) :: ndet, nsamp, di, si, nproc, id, ic, i, j, k, l, nj
 	integer(4) :: steps(size(rbox,1)), pix(2,bz)
 	real(4)    :: x0(size(rbox,1)), inv_dx(size(rbox,1)), phase(size(map,3),bz)
 	real(4)    :: ipoint(size(bore,1),bz), opoint(size(ys,1),bz)
@@ -56,7 +63,9 @@ subroutine map_nearest( &
 		!$omp end parallel workshare
 	end if
 
-	!$omp parallel do collapse(2) private(di,si,nj,j,ipoint,opoint,pix)
+	!$omp parallel private(di,si,nj,j,ipoint,opoint,pix,id)
+	id = omp_get_thread_num()+1
+	!$omp do collapse(2)
 	do di = 1, ndet
 		do si = 1, nsamp, bz
 			nj = min(bz,nsamp-si+1)
@@ -64,7 +73,7 @@ subroutine map_nearest( &
 				ipoint(:,j) = bore(:,si+j-1)+det_pos(:,di)
 			enddo
 			opoint(:,:nj)  = lookup_grad(ipoint(:,:nj), x0, inv_dx, steps, ys)
-			pix(:,:nj)    = floor(opoint(1:2,:nj))+1 ! fortran index offset
+			pix(:,:nj)    = nint(opoint(1:2,:nj))+1 ! fortran index offset
 			! Bounds check (<1% cost)
 			do j = 1, nj
 				pix(1,j) = min(size(map,2),max(1,pix(1,j)))
@@ -73,7 +82,7 @@ subroutine map_nearest( &
 			phase(:,:nj)  = get_phase(comps, det_comps(:,di), opoint(3:,:nj))
 			if(dir < 0) then
 				do j = 1, nj
-					map(pix(2,j),pix(1,j),:) = map(pix(2,j),pix(1,j),:) + tod(si+j-1,di)*phase(:,j)
+					wmap(pix(2,j),pix(1,j),:,id) = wmap(pix(2,j),pix(1,j),:,id) + tod(si+j-1,di)*phase(:,j)
 				end do
 			else
 				do j = 1, nj
@@ -82,8 +91,9 @@ subroutine map_nearest( &
 			end if
 		end do
 	end do
+	!$omp end parallel
 
-	if(dir > 0) then
+	if(dir < 0) then
 		!$omp parallel do collapse(3)
 		do j = 1, size(wmap, 3)
 			do k = 1, size(wmap, 2)
@@ -97,6 +107,111 @@ subroutine map_nearest( &
 	end if
 end subroutine
 
+subroutine pmat_linear( &
+		dir,                              &! Direction of the projection: 1: forward (map2tod), -1: backard (tod2map)
+		tod, map,                         &! Main inputs/outpus
+		bore, det_pos, det_comps,  comps, &! Input pointing
+		rbox, nbox, ys                    &! Coordinate transformation
+	)
+	use omp_lib
+	implicit none
+	! Parameters
+	integer(4), intent(in)    :: dir, comps(:), nbox(:)
+	real(4),    intent(in)    :: bore(:,:), ys(:,:,:) ! comp, derivs, pix
+	real(4),    intent(in)    :: det_pos(:,:), det_comps(:,:), rbox(:,:)
+	real(4),    intent(inout) :: tod(:,:), map(:,:,:)
+	! Work
+	integer(4), parameter :: bz = 321
+	integer(4) :: ndet, nsamp, di, si, nproc, id, ic, i, j, k, l, nj, py,px
+	integer(4) :: steps(size(rbox,1)), pix(2,bz)
+	real(4)    :: x0(size(rbox,1)), inv_dx(size(rbox,1)), phase(size(map,3),bz)
+	real(4)    :: ipoint(size(bore,1),bz), opoint(size(ys,1),bz)
+	real(4)    :: mt(2,2), fpix(2,bz)
+	real(4), allocatable :: wmap(:,:,:,:)
+
+	nsamp   = size(tod, 1)
+	ndet    = size(tod, 2)
+
+	! In C order, ys has pixel axes t,ra,dec, so nbox = [nt,nra,ndec]
+	! Index mapping is therefore given by [nra*ndec,ndec,1]
+	steps(size(steps)) = 1
+	do ic = size(steps)-1, 1, -1
+		steps(ic) = steps(ic+1)*nbox(ic+1)
+	end do
+	x0 = rbox(:,1); inv_dx = nbox/(rbox(:,2)-rbox(:,1))
+
+	if(dir < 0) then
+		nproc = omp_get_max_threads()
+		allocate(wmap(size(map,1),size(map,2),size(map,3),nproc))
+		!$omp parallel workshare
+		wmap = 0
+		!$omp end parallel workshare
+	end if
+
+	!$omp parallel private(di,si,nj,j,ipoint,opoint,pix,id)
+	id = omp_get_thread_num()+1
+	!$omp do collapse(2)
+	do di = 1, ndet
+		do si = 1, nsamp, bz
+			nj = min(bz,nsamp-si+1)
+			do j = 1, nj
+				ipoint(:,j) = bore(:,si+j-1)+det_pos(:,di)
+			enddo
+			opoint(:,:nj) = lookup_grad(ipoint(:,:nj), x0, inv_dx, steps, ys)
+			pix(:,:nj)    = floor(opoint(1:2,:nj))
+			fpix(:,:nj)   = opoint(1:2,:nj)-pix(:,:nj)
+			pix(:,:nj)    = pix(:,:nj)+1 ! fortran pixel offset
+			! Bounds check (<1% cost)
+			phase(:,:nj)  = get_phase(comps, det_comps(:,di), opoint(3:,:nj))
+			if(dir < 0) then
+				! Different bounds check in each direction, since the offset is -1
+				! for tod2map and +1 for map2tod
+				do j = 1, nj
+					pix(1,j) = min(size(map,2),max(2,pix(1,j)))
+					pix(2,j) = min(size(map,1),max(2,pix(2,j)))
+				end do
+				do j = 1, nj
+					py = pix(2,j); px = pix(1,j)
+					wmap(py-0,px-0,:,id) = wmap(py-0,px-0,:,id) + (1 + fpix(1,j)*fpix(2,j) - fpix(1,j)-fpix(2,j))*phase(:,j)
+					wmap(py-1,px-0,:,id) = wmap(py-1,px-0,:,id) + (fpix(1,j)*(1-fpix(2,j)))*phase(:,j)
+					wmap(py-0,px-1,:,id) = wmap(py-0,px-1,:,id) + (fpix(2,j)*(1-fpix(1,j)))*phase(:,j)
+					wmap(py-1,px-1,:,id) = wmap(py-1,px-1,:,id) + (fpix(1,j)*fpix(2,j))*phase(:,j)
+				end do
+			else
+				! Bounds check, see above
+				do j = 1, nj
+					pix(1,j) = min(size(map,2)-1,max(1,pix(1,j)))
+					pix(2,j) = min(size(map,1)-1,max(1,pix(2,j)))
+				end do
+				do j = 1, nj
+					py = pix(2,j); px = pix(1,j)
+					tod(si+j-1,di) = sum((&
+						map(py+0,px+0,:)*(1-fpix(1,j)-fpix(2,j)+fpix(1,j)*fpix(2,j)) + &
+						map(py+1,px+0,:)*(fpix(1,j)*(1-fpix(2,j))) + &
+						map(py+0,px+0,:)*(fpix(2,j)*(1-fpix(1,j))) + &
+						map(py+1,px+1,:)*(fpix(1,j)*fpix(2,j)))*phase(:,j))
+				end do
+			end if
+		end do
+	end do
+	!$omp end parallel
+
+	if(dir < 0) then
+		!$omp parallel do collapse(3)
+		do j = 1, size(wmap, 3)
+			do k = 1, size(wmap, 2)
+				do l = 1, size(wmap, 1)
+					do i = 1, size(wmap,4)
+						map(l,k,j) = map(l,k,j) + wmap(l,k,j,i)
+					end do
+				end do
+			end do
+		end do
+	end if
+
+end subroutine
+
+! Trilinear is 35% slower for almost no gain, it seems.
 !function lookup_trilinear(ipoint, x0, inv_dx, steps, ys) result(opoint)
 !	implicit none
 !	real(4),    intent(in) :: ipoint(:,:), x0(:), inv_dx(:)
