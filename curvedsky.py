Attempted to fix the off-by-one pixel problem. It seems to work now, but wcs stuff is error prone. I've tested box, extent, upgrade, slicing, sky2pix, pix2sky, and they all seem to work. I've also tested ds9, and it now agrees with enlib. So hopefully this is correct.

curvedsky.py
@@ -9,7 +9,7 @@ def rand_map(shape, wcs, ps, lmax=None, dtype=np.float64, seed=None, oversample=
 	that it takes into account the curvature of the full sky. This makes it much
 	slower and more memory-intensive. The map should not cross the poles."""
 	# Ensure everything has the right dimensions and restrict to relevant dimensions
-	ps = np.atleast_3d(ps)
+	ps = utils.atleast_3d(ps)
 	assert ps.shape[0] == ps.shape[1], "ps must be [ncomp,ncomp,nl] or [nl]"
 	assert len(shape) == 2 or len(shape) == 3, "shape must be (ncomp,ny,nx) or (ny,nx)"
 	ncomp = 1 if len(shape) == 2 else shape[-2]

enmap.py
@@ -108,7 +108,7 @@ class ndmap(np.ndarray):
 		# pixel-center coordinates to pixel-edge coordinates,
 		# which we need to distinguish between fully or partially
 		# included pixels
-		bpix = self.wcs.wcs_world2pix(box[:,::-1]*180/np.pi,1)[:,::-1]+0.5
+		bpix = self.wcs.wcs_world2pix(box[:,::-1]*180/np.pi,0)[:,::-1]+0.5
 		dir  = 2*(bpix[1]>bpix[0])-1
 		# If we are inclusive, find a bounding box, otherwise,
 		# an internal box
@@ -126,10 +126,10 @@ def slice_wcs(shape, wcs, sel):
 		s = enlib.slice.expand_slice(s, shape[i])
 		j = -1-i
 		start = s.start if s.step > 0 else s.start + 1
-		wcs.wcs.crpix[j] -= start-0.5
+		wcs.wcs.crpix[j] -= start+0.5
 		wcs.wcs.crpix[j] /= s.step
 		wcs.wcs.cdelt[j] *= s.step
-		wcs.wcs.crpix[j] -= 0.5
+		wcs.wcs.crpix[j] += 0.5
 		oshape[i] = (oshape[i]+s.step-1)/s.step
 	return tuple(oshape), wcs
 
@@ -139,7 +139,7 @@ def box(shape, wcs, npoint=10):
 	# extra pixels to make our unwinding unambiguous
 	pix = np.array([np.linspace(0,shape[-2],num=npoint,endpoint=True),
 		np.linspace(0,shape[-1],num=npoint,endpoint=True)])-0.5
-	coords = wcs.wcs_pix2world(pix[1],pix[0],1)[::-1]
+	coords = wcs.wcs_pix2world(pix[1],pix[0],0)[::-1]
 	return enlib.utils.unwind(np.array(coords)*np.pi/180).T[[0,-1]]
 
 def enmap(arr, wcs=None, dtype=None, copy=True):
@@ -190,7 +190,7 @@ def pix2sky(shape, wcs, pix, safe=True, corner=False):
 	pix = np.asarray(pix).astype(float)
 	if corner: pix -= 0.5
 	pflat = pix.reshape(pix.shape[0], np.prod(pix.shape[1:]))
-	coords = np.asarray(wcs.wcs_pix2world(*(tuple(pflat)[::-1]+(1,)))[::-1])*np.pi/180
+	coords = np.asarray(wcs.wcs_pix2world(*(tuple(pflat)[::-1]+(0,)))[::-1])*np.pi/180
 	coords = coords.reshape(pix.shape)
 	if safe: coords = enlib.utils.unwind(coords)
 	return coords
@@ -205,7 +205,7 @@ def sky2pix(shape, wcs, coords, safe=True, corner=False):
 	coords = np.asarray(coords)*180/np.pi
 	cflat  = coords.reshape(coords.shape[0], np.prod(coords.shape[1:]))
 	# Quantities with a w prefix are in wcs ordering (ra,dec)
-	wpix = np.asarray(wcs.wcs_world2pix(*tuple(cflat)[::-1]+(1,)))
+	wpix = np.asarray(wcs.wcs_world2pix(*tuple(cflat)[::-1]+(0,)))
 	wshape = shape[-2:][::-1]
 	if corner: wpix += 0.5
 	if safe:
@@ -276,8 +276,9 @@ def extent(shape, wcs, nsub=0x10):
 	patch given by shape and wcs as [height,width] in
 	radians. That is, if the patch were on a sphere with
 	radius 1 m, then this function returns approximately how many meters
-	tall and width the patch is. These are defined such that
+	tall and wide the patch is. These are defined such that
 	their product equals the physical area of the patch."""
+	# Create a new wcs with (nsub,nsub) pixels
 	wcs = wcs.deepcopy()
 	step = (np.asfarray(shape[-2:])/nsub)[::-1]
 	wcs.wcs.cdelt *= step
@@ -392,7 +393,7 @@ def samewcs(arr, *args):
 # Use that to make everything that currently accepts shape, wcs
 # transparently accept geometry. This will free us from having
 # to drag around a shape, wcs pair all the time.
-def geometry(pos, res=None, shape=None, proj="cea", **kwargs):
+def geometry(pos, res=None, shape=None, proj="cea", deg=False, **kwargs):
 	"""Consruct a shape,wcs pair suitable for initializing enmaps.
 	pos can be either a [2] center position or a [{from,to},2]
 	bounding box. At least one of res or shape must be specified.
@@ -405,7 +406,7 @@ def geometry(pos, res=None, shape=None, proj="cea", **kwargs):
 	wcs = enlib.wcs.build(pos, res, shape, rowmajor=True, system=proj, **kwargs)
 	if shape is None:
 		# Infer shape
-		corners = wcs.wcs_world2pix(pos[:,::-1],1)
+		corners = wcs.wcs_world2pix(pos[:,::-1],0)
 		shape = tuple(np.ceil(np.abs(corners[1]-corners[0])).astype(int))[::-1]
 	return tuple(shape), wcs
 
@@ -601,10 +602,10 @@ def upgrade(emap, factor):
 	res = res.reshape(res.shape[:-4]+(np.product(res.shape[-4:-2]),np.product(res.shape[-2:])))
 	# Correct the WCS information
 	for j in range(2):
-		res.wcs.wcs.crpix[j] += 0.5
+		res.wcs.wcs.crpix[j] -= 0.5
 		res.wcs.wcs.crpix[j] *= factor[1-j]
 		res.wcs.wcs.cdelt[j] /= factor[1-j]
-		res.wcs.wcs.crpix[j] -= 0.5
+		res.wcs.wcs.crpix[j] += 0.5
 	return res
 
 def pad(emap, pix, return_slice=False,wrap=False):

wcs.py
@@ -12,6 +12,11 @@ from enlib import slice
 # have to be 1 rather than the 0 one would expect. For example,
 # if wcs is CAR(crval=(0,0),crpix=(0,0),cdelt=(1,1)), then
 # pix2world(0,0,1) is (0,0) while pix2world(0,0,0) is (-1,-1).
+#
+# No! the problem is that everythin in the fits header counts from 1,
+# so the default crpix should be (1,1), not (0,0). With
+# CAR(crval(0,0),crpix(1,1),cdelt(1,1)) we get
+# pix2world(1,1,1) = (0,0) and pix2world(0,0,0) = (0,0)
 
 # Useful stuff to be able to do:
 #  * Create a wcs from (point,res)
@@ -33,6 +38,19 @@ from enlib import slice
 
 deg2rad = np.pi/180
 rad2deg = 1/deg2rad
+
+def describe(wcs):
+	"""Since astropy.wcs.WCS objects do not have a useful
+	str implementation, this function provides a relpacement."""
+	sys  = wcs.wcs.ctype[0][-3:].lower()
+	n    = wcs.naxis
+	fields = ("cdelt:["+",".join(["%.3g"]*n)+"],crval:["+",".join(["%.3g"]*n)+"],crpix:["+",".join(["%.3g"]*n)+"]") % (tuple(wcs.wcs.cdelt) + tuple(wcs.wcs.crval) + tuple(wcs.wcs.crpix))
+	pv = wcs.wcs.get_pv()
+	for p in pv:
+		fields += ",pv[%d,%d]=%.3g" % p
+	return "%s:{%s}" % (sys, fields)
+# Add this to all WCSes in this class
+WCS.__repr__ = describe
 
 def car(pos, res=None, shape=None, rowmajor=False, ref=None):
 	"""Set up a plate carree system. See the build function for details."""
@@ -116,7 +134,7 @@ def validate(pos, res, shape, rowmajor=False):
 def finalize(w, pos, res, shape, ref=None):
 	"""Common logic for the various wcs builders. Fills in the reference
 	pixel and resolution."""
-	w.wcs.crpix = [0,0]
+	w.wcs.crpix = [1,1]
 	if res is None:
 		# Find the resolution that gives our box the required extent.
 		w.wcs.cdelt = [1,1]
@@ -128,10 +146,10 @@ def finalize(w, pos, res, shape, ref=None):
 	if pos.ndim == 1:
 		if shape is not None:
 			# Place pixel origin at corner of shape centered on crval
-			w.wcs.crpix = np.array(shape)/2-0.5
+			w.wcs.crpix = np.array(shape)/2.0+0.5
 	else:
-		# Make pos[0] the corner of the (0,0) pixel
-		off = w.wcs_world2pix(pos[0,None],1)[0]+0.5
+		# Make pos[0] the corner of the (0,0) pixel (counting from 0 for simplicity)
+		off = w.wcs_world2pix(pos[0,None],0)[0]+0.5
 		w.wcs.crpix -= off
 	if ref is not None:
 		# Tweak wcs so that crval is an integer number of pixels
@@ -139,7 +157,7 @@ def finalize(w, pos, res, shape, ref=None):
 		# on ref, measuring the pixel coordinates of crval in this system
 		# and truncating it to a whole pixel number.
 		wtmp = w.deepcopy()
-		wtmp.wcs.crpix = (0,0)
+		wtmp.wcs.crpix = (1,1)
 		wtmp.wcs.crval = ref
 		w.wcs.crval = wtmp.wcs_pix2world(np.round(wtmp.wcs_world2pix(w.wcs.crval[None],1)),1)[0]
 		# We can then simply round the crpix to the closest integer. Together with the
@@ -147,13 +165,6 @@ def finalize(w, pos, res, shape, ref=None):
 		# cost one has to pay for this realignment.
 		w.wcs.crpix = np.round(w.wcs.crpix)
 	return w
-
-def describe(wcs):
-	"""Since astropy.wcs.WCS objects do not have a useful
-	str implementation, this function provides a relpacement."""
-	sys  = wcs.wcs.ctype[0][-3:].lower()
-	n    = wcs.naxis
-	return ("%s:{cdelt:["+",".join(["%.3g"]*n)+"],crval:["+",".join(["%.3g"]*n)+"],crpix:["+",".join(["%.3g"]*n)+"]}") % ((sys,) + tuple(wcs.wcs.cdelt) + tuple(wcs.wcs.crval) + tuple(wcs.wcs.crpix))
 
 def angdist(lon1,lat1,lon2,lat2):
 	return np.arccos(np.cos(lat1)*np.cos(lat2)*(np.cos(lon1)*np.cos(lon2)+np.sin(lon1)*np.sin(lon2))+np.sin(lat1)*np.sin(lat2))