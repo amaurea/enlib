8b5dd13058954f94218d3b9128686d4922c3396f
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2015-02-12 19:34:03 +0200

    Added hippo.mk for compilation on hippo

---
+++ compile_opts/hippo.mk
@@ -0,0 +1,6 @@
+export LAPACK_LINK = -llapack -lopenblas
+export OMP_LINK    = -lgomp
+export FFLAGS      = -fopenmp -O3 -ffast-math -fPIC -ffree-line-length-none
+export FC          = gfortran
+export F2PY        = f2py
+export F2PYCOMP    = gfortran
