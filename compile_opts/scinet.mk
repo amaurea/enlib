03dbfbb70021c55c6d376af5892d4ccd04c00258
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2016-02-26 14:16:49 -0500

    Working on new pmat stuff.

--- compile_opts/scinet.mk
+++ compile_opts/scinet.mk
@@ -1,6 +1,6 @@
 export LAPACK_LINK = -L$(MKLPATH) -lmkl_rt -lpthread -lm
 export OMP_LINK    = -liomp5
-export FFLAGS      = -openmp -Ofast -fPIC -xhost -vec-report -opt-report
+export FFLAGS      = -openmp -Ofast -fPIC -xhost # -vec-report -opt-report
 export FSAFE       = -openmp -O3 -fPIC -xhost
 export FC          = ifort
 export F2PY        = f2py
