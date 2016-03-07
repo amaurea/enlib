33e0a564f35a06f2b6b0b9e87e81106a810bc6a2
Sigurd Næss <sigurdkn@astro.uio.no>
2016-03-07 12:36:32 +0000

    Fixed glamdring.mk

--- compile_opts/glamdring.mk
+++ compile_opts/glamdring.mk
@@ -1,9 +1,10 @@
 export LAPACK_LINK = -L$(MKLROOT)/lib/intel64 -lmkl_intel_lp64 -lmkl_avx -lmkl_def -lmkl_core -lmkl_intel_thread -lpthread -lm
 export OMP_LINK    = -liomp5
-export F90FLAGS    = -openmp -Ofast -fPIC
-#export F90FLAGS    = -O0 -g -debug all -check bounds -fPIC
-export FFLAGS      = $(F90FLAGS)
+export FFLAGS    = -openmp -Ofast -fPIC
+#export FFLAGS    = -O0 -g -debug all -check bounds -fPIC
 export FC          = ifort
 export F2PY        = f2py
 export F2PYCOMP    = intelem
-
+export PYTHON      = python
+export SED         = sed
+export CC          = icc
