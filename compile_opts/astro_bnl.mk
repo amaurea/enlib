5181f19b1e5c5f67b146c60c6090b829a9f9b5a9
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-06-16 22:19:13 -0400

    make file for astro bnl

---
+++ compile_opts/astro_bnl.mk
@@ -0,0 +1,10 @@
+export LAPACK_LINK = -L$(MKLPATH) -lmkl_rt -lpthread -lm
+export OMP_LINK    = -liomp5
+export FFLAGS      = -openmp -Ofast -fPIC -xhost # -vec-report -opt-report
+export FSAFE       = -openmp -O3 -fPIC -xhost
+export FC          = ifort
+export F2PY        = f2py
+export F2PYCOMP    = intelem
+export PYTHON      = python
+export SED         = sed
+export CC          = icc
