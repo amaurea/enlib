Add point source map simulation. Fix some bugs in some map2alm functions that prevented them from running. Off-by-one pixel in fullsky geometry.

curvedsky.py
@@ -244,14 +244,14 @@ def map2alm_cyl(map, alm=None, ainfo=None, lmax=None, spin=2, direct=False,
 	minfo = match_predefined_minfo(tmap, rtol=rtol, atol=atol)
 	return map2alm_raw(tmap, alm, minfo, ainfo, spin=spin)
 
-def map2alm_healpix(healmap, alm=None, ainfo=None, lmax=None, spin=2, deriv=False, copy=False):
+def map2alm_healpix(healmap, alm=None, ainfo=None, lmax=None, spin=2, copy=False):
 	"""Projects the given alm[...,ncomp,nalm] onto the given healpix map
 	healmap[...,ncomp,npix]."""
 	alm, ainfo = prepare_alm(alm, ainfo, lmax, healmap.shape[:-1], healmap.dtype)
 	nside = npix2nside(healmap.shape[-1])
 	minfo = sharp.map_info_healpix(nside)
 	return map2alm_raw(healmap[...,None], alm, minfo=minfo, ainfo=ainfo,
-			spin=spin, deriv=deriv, copy=copy)
+			spin=spin, copy=copy)
 
 ######################
 ### Raw transforms ###
@@ -461,7 +461,7 @@ def match_predefined_minfo(m, rtol=None, atol=None):
 	return minfo_cut
 
 def npix2nside(npix):
-	return utils.nint((healmap.shape[-1]/12)**0.5)
+	return utils.nint((npix/12)**0.5)
 
 def prepare_alm(alm=None, ainfo=None, lmax=None, pre=(), dtype=np.float64):
 	"""Set up alm and ainfo based on which ones of them are available."""

enmap.py
@@ -670,11 +670,13 @@ def fullsky_geometry(res=None, shape=None, dims=(), proj="car"):
 	if shape is None:
 		res   = np.zeros(2)+res
 		shape = ([1*np.pi,2*np.pi]/res+0.5).astype(int)
+		shape[0] += 1
 	ny,nx = shape
+	ny   -= 1
 	wcs   = enlib.wcs.WCS(naxis=2)
 	wcs.wcs.crval = [0,0]
 	wcs.wcs.cdelt = [-360./nx,180./ny]
-	wcs.wcs.crpix = [nx/2+1,ny/2+1]
+	wcs.wcs.crpix = [nx/2.+1,ny/2.+1]
 	wcs.wcs.ctype = ["RA---CAR","DEC--CAR"]
 	return dims+(ny+1,nx+0), wcs
 
@@ -822,7 +824,7 @@ def upgrade(emap, factor):
 		res.wcs.wcs.crpix[j] += 0.5
 	return res
 
-def pad(emap, pix, return_slice=False,wrap=False):
+def pad(emap, pix, return_slice=False, wrap=False):
 	"""Pad enmap "emap", creating a larger map with zeros filled in on the sides.
 	How much to pad is controlled via pix. If pix is a scalar, it specifies the number
 	of pixels to add on all sides. If it is 1d, it specifies the number of pixels to add

pmat/pmat.py
@@ -8,7 +8,7 @@ to incrementally project different parts of the signal.
 """
 import numpy as np, time, sys
 from enlib import enmap, interpol, utils, coordinates, config, errors, array_ops
-from enlib import parallax, bunch
+from enlib import parallax, bunch, pointsrcs
 import pmat_core_32
 import pmat_core_64
 def get_core(dtype):
@@ -506,38 +506,43 @@ class PmatPtsrc(PointingMatrix):
 		ipol, obox, err = build_interpol(transform, scan.box, scan.entry.id, posunit=0.5*utils.arcsec)
 		self.rbox, self.nbox, self.yvals = extract_interpol_params(ipol, srcs.dtype)
 
-		# Build source hit grid
-		cbox    = obox[:,:2]
-		cshape  = tuple(np.ceil(((cbox[1]-cbox[0])/cres)).astype(int))
-		self.ref = np.mean(cbox,0)
-		srcs[...,:2] = utils.rewind(srcs[...,:2], self.ref)
+		self.cbox = obox[:,:2]
+		self.ref  = np.mean(self.cbox,0)
+		self.ncell, self.cells = pointsrcs.build_src_cells(self.cbox, srcs[...,:2], cres, unwind=True)
 
-		# A cell is hit if it overlaps both horizontally and vertically
-		# with the point source +- rmax
-		# ncell is [ndir,nsrc_det,ncy,ncx]
-		ncell = np.zeros((ndir,src_ndet)+cshape,dtype=np.int32)
-		# cells is [ndir,nsrc_det,ncy,ncx,maxcell]
-		cells = np.zeros((ndir,src_ndet)+cshape+(maxcell,),dtype=np.int32)
-		c0 = cbox[0]; inv_dc = cshape/(cbox[1]-cbox[0])
-		for si in range(nsrc):
-			for sdir in range(ndir):
-				for sdi in range(src_ndet):
-					src = srcs[si,sdir,sdi]
-					i1 = (src[:2]-rmax-c0)*inv_dc
-					i2 = (src[:2]+rmax-c0)*inv_dc+1 # +1 because this is a half-open interval
-					# Truncate to edges - any source outside of our region
-					# will be put on one of the edge cells
-					i1 = np.maximum(i1.astype(int), 0)
-					i2 = np.minimum(i2.astype(int), np.array(cshape)-1)
-					#print si, sdir, i1, i2, cshape
-					if np.any(i1 >= cshape) or np.any(i2 < 0): continue
-					sel= (sdir,sdi,slice(i1[0],i2[0]),slice(i1[1],i2[1]))
-					cells[sel][:,:,ncell[sel]] = si
-					ncell[sel] += 1
+		## Build source hit grid
+		#cbox    = obox[:,:2]
+		#cshape  = tuple(np.ceil(((cbox[1]-cbox[0])/cres)).astype(int))
+		#self.ref = np.mean(cbox,0)
+		#srcs[...,:2] = utils.rewind(srcs[...,:2], self.ref)
 
-		self.cells, self.ncell = cells, ncell
+		## A cell is hit if it overlaps both horizontally and vertically
+		## with the point source +- rmax
+		## ncell is [ndir,nsrc_det,ncy,ncx]
+		#ncell = np.zeros((ndir,src_ndet)+cshape,dtype=np.int32)
+		## cells is [ndir,nsrc_det,ncy,ncx,maxcell]
+		#cells = np.zeros((ndir,src_ndet)+cshape+(maxcell,),dtype=np.int32)
+		#c0 = cbox[0]; inv_dc = cshape/(cbox[1]-cbox[0])
+		#for si in range(nsrc):
+		#	for sdir in range(ndir):
+		#		for sdi in range(src_ndet):
+		#			src = srcs[si,sdir,sdi]
+		#			i1 = (src[:2]-rmax-c0)*inv_dc
+		#			i2 = (src[:2]+rmax-c0)*inv_dc+1 # +1 because this is a half-open interval
+		#			# Truncate to edges - any source outside of our region
+		#			# will be put on one of the edge cells
+		#			i1 = np.maximum(i1.astype(int), 0)
+		#			i2 = np.minimum(i2.astype(int), np.array(cshape)-1)
+		#			#print si, sdir, i1, i2, cshape
+		#			if np.any(i1 >= cshape) or np.any(i2 < 0): continue
+		#			sel= (sdir,sdi,slice(i1[0],i2[0]),slice(i1[1],i2[1]))
+		#			cells[sel][:,:,ncell[sel]] = si
+		#			ncell[sel] += 1
+
+		#self.cells, self.ncell = cells, ncell
+		#print self.cells.shape, self.ncell.shape
+
 		self.rmax = rmax
-		self.cbox = cbox
 		self.tmul = 1 if tmul is None else tmul
 		self.pmul = 1 if pmul is None else pmul
 		self.err  = err
@@ -578,7 +583,7 @@ def build_interpol(transform, box, id="none", posunit=1.0, sys=None):
 	# Build pointing interpolator
 	errlim = np.array([1e-3*posunit,1e-3*posunit,utils.arcmin,utils.arcmin])*acc
 	ipol, obox, ok, err = interpol.build(transform, interpol.ip_linear, box, errlim, maxsize=ip_size, maxtime=ip_time, return_obox=True, return_status=True)
-	if not ok: print "Warning: Accuracy %g was specified, but only reached %g for tod %s" % (acc, np.max(err/errlim)*acc, id)
+	if not ok and np.any(err>errlim): print "Warning: Accuracy %g was specified, but only reached %g for tod %s" % (acc, np.max(err/errlim)*acc, id)
 	return ipol, obox, err
 
 def build_pos_transform(scan, sys):