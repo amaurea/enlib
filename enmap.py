Fix off-by-one error in sampcut.gapfill_const, and make it safer at the same time. Mak sure tele and bore have the right handedness. Add enmap.shift. Bugfixes in mapmaking.calc_icov_map. Some debug stuff. Made 5 s the default tod window, since that's what I've been using in config files for a while.

enmap.py
@@ -134,8 +134,6 @@ class ndmap(np.ndarray):
 	def write(self, fname, fmt=None):
 		write_map(fname, self, fmt=fmt)
 
-
-
 def subinds(shape, wcs, box, inclusive=False, cap=True):
 	"""Helper function for submap. Translates the bounding
 	box provided into a pixel units. Assumes rectangular
@@ -1366,4 +1364,11 @@ def fix_endian(map):
 	Returns the result."""
 	if map.dtype.byteorder not in ['=','<' if sys.byteorder == 'little' else '>']:
 		map = map.byteswap(True).newbyteorder()
+	return map
+
+def shift(map, off):
+	off = np.atleast_1d(off)
+	for i, o in enumerate(off):
+		if o != 0:
+			map[:] = np.roll(map, o, -len(off)+i)
 	return map

flagrange.py
@@ -328,7 +328,7 @@ def fill_right(inds, vals, n):
 	inds, vals = np.asarray(inds), np.asarray(vals)
 	# Add default start condition of 0
 	inds  = np.concatenate([[0],inds,[n]])
-	vals  = np.concatenate([[0],vals])
+	vals  = np.concatenate([np.array([0],vals.dtype),vals])
 	nums  = inds[1:]-inds[:-1]
 	return np.repeat(vals, nums)
 

mapmaking.py
@@ -577,6 +577,7 @@ def calc_icov_map(signal, scans, pos, weights, signal_cut=None):
 	operation, so if the points are too close to each other their covariance
 	information will interfere with each other."""
 	icov   = signal.zeros()
+	ocov   = icov.copy()
 	# Set the chosen positions to one. This is very inelegant. Should have
 	# global pixel setting methods in dmap
 	pos    = np.zeros([1,2],int)+pos # broadcast to correct shape
@@ -590,13 +591,14 @@ def calc_icov_map(signal, scans, pos, weights, signal_cut=None):
 				if y >= 0 and x >= 0 and y < tile.shape[-2] and x < tile.shape[-1]:
 					tile[0,y,x] = 1
 	# The rest proceeds similarly to the crosslinking map
-	owork   = signal.prepare(icov)
+	iwork   = signal.prepare(icov)
+	owork   = signal.prepare(ocov)
 	if signal_cut is not None: ojunk = signal_cut.prepare(signal_cut.zeros())
 	for scan in scans:
 		with bench.mark("icov_Pr_" + signal.name): signal.precompute(scan)
 		with bench.mark("icov_P_" + signal.name):
-			tod = np.full((scan.ndet, scan.nsamp), 1.0, signal.dtype)
-			signal.forward(scan, tod, owork)
+			tod = np.zeros((scan.ndet, scan.nsamp), signal.dtype)
+			signal.forward(scan, tod, iwork)
 			if signal_cut is not None: signal_cut.forward(scan, tod, ojunk)
 		with bench.mark("icov_nmat"):
 			for weight in weights: weight(scan, tod)
@@ -608,8 +610,8 @@ def calc_icov_map(signal, scans, pos, weights, signal_cut=None):
 		with bench.mark("icov_Fr_" + signal.name): signal.free()
 		times = [bench.stats[s]["time"].last for s in ["icov_P_" + signal.name, "icov_nmat", "icov_PT_" + signal.name]]
 		L.debug("div %s %6.3f %6.3f %6.3f %s" % ((signal.name,)+tuple(times)+(scan.id,)))
-	signal.finish(icov, owork)
-	return icov[0]
+	signal.finish(ocov, owork)
+	return ocov[0]
 
 def calc_hits_map(hits, signal, signal_cut, scans):
 	work = signal.prepare(hits)
@@ -724,6 +726,7 @@ class FilterPickup:
 		else:
 			waz = (np.max(scan.boresight[:,1])-np.min(scan.boresight[:,1]))/utils.degree
 			naz = utils.nint(waz/self.daz)
+			print "A", self.daz, waz, naz
 		todfilter.filter_poly_jon(tod, scan.boresight[:,1], hwp=scan.hwp, naz=naz, nt=self.nt, nhwp=self.nhwp, niter=self.niter, cuts=scan.cut)
 
 class FilterHWPNotch:
@@ -940,7 +943,7 @@ class Eqsys:
 		self.weights = weights
 		self.dof     = zipper.MultiZipper([signal.dof for signal in signals], comm=comm)
 		self.b       = None
-	def A(self, x):
+	def A(self, x, debug_file=None):
 		"""Apply the A-matrix P'N"P to the zipped vector x, returning the result."""
 		with bench.mark("A_init"):
 			imaps  = self.dof.unzip(x)
@@ -950,7 +953,7 @@ class Eqsys:
 			iwork = [signal.prepare(map) for signal, map in zip(self.signals, imaps)]
 			owork = [signal.work() for signal in self.signals]
 			#owork = [signal.prepare(map) for signal, map in zip(self.signals, omaps)]
-		for scan in self.scans:
+		for si, scan in enumerate(self.scans):
 			# Set up a TOD for this scan
 			tod = np.zeros([scan.ndet, scan.nsamp], self.dtype)
 			# Project each signal onto the TOD (P) in reverse order. This is done
@@ -961,6 +964,13 @@ class Eqsys:
 						signal.precompute(scan)
 					with bench.mark("A_P_" + signal.name):
 						signal.forward(scan, tod, work)
+			if debug_file is not None and si == 0:
+				print "Eqsys A dumping debug"
+				with h5py.File(debug_file,"w") as hfile:
+					hfile["tod"]  = tod[:16]
+					hfile["mask"] = scan.cut[:16].to_mask()
+					hfile["dets"] = scan.dets[:16]
+					hfile["id"]   = scan.id
 			# Apply the noise matrix (N")
 			with bench.mark("A_N"):
 				for weight in self.weights: weight(scan, tod)
@@ -1023,7 +1033,7 @@ class Eqsys:
 			#dump("dump_prenoise.hdf", tod[:32])
 			with bench.mark("b_N_build"):
 				scan.noise = scan.noise.update(tod, scan.srate)
-				#print "FIXME"
+				#print "FIXME gapfill const after building noise model", scan.id, scan.cut.ndet, scan.cut.nsamp, tod.shape
 				#sampcut.gapfill_const(scan.cut, tod, 0.0, True)
 			with bench.mark("b_filter2"):
 				for filter in self.filters2: filter(scan, tod)

pmat/pmat.py
@@ -24,8 +24,7 @@ config.default("pmat_accuracy",     1.0, "Factor by which to lower accuracy requ
 config.default("pmat_interpol_max_size", 100000, "Maximum mesh size in pointing interpolation. Worst-case time and memory scale at most proportionally with this.")
 config.default("pmat_interpol_max_time", 50, "Maximum time to spend in pointing interpolation constructor. Actual time spent may be up to twice this.")
 config.default("pmat_interpol_pad", 5.0, "Number of arcminutes to pad the interpolation coordinate system by")
-# Disable window for now - it is buggy
-config.default("tod_window",        0.0, "Seconds by which to window each end of the TOD.")
+config.default("tod_window",        5.0, "Seconds by which to window each end of the TOD.")
 
 class PointingMatrix:
 	def forward(self, tod, m): raise NotImplementedError
@@ -440,13 +439,12 @@ class pos2pix:
 		shape = ipos.shape[1:]
 		ipos  = ipos.reshape(ipos.shape[0],-1)
 		time  = self.scan.mjd0 + ipos[0]/utils.day2sec
-		if "bore" in self.sys or "sidelobe" in self.sys:
-			# Need to extract the boresight pointing for the time in question
-			bore = ipos[1:].copy()
-			bore[0] = np.interp(ipos[0], self.scan.boresight[:,0], self.scan.boresight[:,1])
-			bore[1] = np.interp(ipos[0], self.scan.boresight[:,0], self.scan.boresight[:,2])
-		else: bore = None
-		opos = coordinates.transform(self.scan.sys, self.sys, ipos[1:], time=time, site=self.scan.site, pol=True, bore=bore)
+		# We support sidelobe mapping by passing the detector pointing "ipos" as the "boresight"
+		# pointing, which is only used in boresight-relative coordinates or sidelobe mapping.
+		# This actually results in a better coordinate system than if we had passed in the actual
+		# boresight, since we don't really want boresight-centered coordinates, we want detector
+		# centered coordinates.
+		opos = coordinates.transform(self.scan.sys, self.sys, ipos[1:], time=time, site=self.scan.site, pol=True, bore=ipos[1:])
 		# Parallax correction
 		sundist = config.get("pmat_parallax_au")
 		if sundist:

resample.py
@@ -23,7 +23,7 @@ def resample_bin(d, factors=[0.5], axes=None):
 
 def downsample_bin(d, steps=[2], axes=None):
 	assert len(steps) <= d.ndim
-	if axes is None: axes = np.arange(-1,-len(steps)-1,-1)
+	if axes is None: axes =range(-len(steps),0)
 	assert len(axes) == len(steps)
 	# Expand steps to cover every axis in order
 	fullsteps = np.zeros(d.ndim,dtype=int)+1