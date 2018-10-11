bc91255f53f010fb813f32a6ee654764de33ba77
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2018-10-10 23:51:07 -0400

    Added tiger.mk

---
+++ compile_opts/tiger.mk
@@ -0,0 +1,13 @@
+export LAPACK_LINK = -L$(MKLROOT)/lib/intel64 -lmkl_rt -lpthread -lm
+export OMP_LINK    = -liomp5
+export FFLAGS      = -qopenmp -Ofast -fPIC -xhost -nofor-main # -vec-report -opt-report
+#export FFLAGS      = -O0 -traceback -check bounds -g -fPIC -xhost -nofor-main # -vec-report -opt-report
+export FSAFE       = -qopenmp -O3 -fPIC -xhost
+export CFLAGS      = -fopenmp -lgomp -fPIC -std=c99
+export FC          = ifort
+export F2PY        = f2py
+export F2PYCOMP    = intelem
+export PYTHON      = python
+export SED         = sed
+export CC          = gcc
+export LDSHARED    = $(CC) -shared
