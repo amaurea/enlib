988aeab3cd284593fd5946c6f88d098851d1a49c
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2020-10-19 12:54:51 -0400

    Fixed missing fields in struct initialization. No practical effect

--- iers/iers2struct.c
+++ iers/iers2struct.c
@@ -33,13 +33,13 @@ int main()
 	{
 		if(fixed) free(fixed);
 		fixed = addspaces(line, fieldwidths, sizeof(fieldwidths)/sizeof(int));
-		n=sscanf(fixed, "%d %d %d %5d.00 %c %lf %lf %lf %lf %c %lf %lf %lf %lf %c %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",
+		n=sscanf(fixed, "%d %d %d %5d.00 %c %lf %lf %lf %lf %c %lf %lf %lf %lf %c %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",
 			&year,&month,&day,&mjd,&ip[0],&pm[0],&epm[0],&pm[1],&epm[1],
 			&ip[1],&dUT,&edUT,&lod,&elod,&ip[2],&dPsi,&edPsi,&dEps,&edEps,
 			&foo[0],&foo[1],&foo[2],&foo[3],&foo[4],&foo[5],&foo[6]);
 		if(n < 12) continue;
 		year += 1900; if(year < 1950) year += 100;
-		printf("	{%4d, %2d, %2d, %5d, '%c', %9.6f, %9.6f, %9.6lf, %9.6lf, '%c', %10.7lf, %10.7lf, %7.4lf, %7.4lf, '%c', %9.3lf, %9.3lf},\n",
+		printf("	{%4d, %2d, %2d, %5d, '%c', %9.6f, %9.6f, %9.6lf, %9.6lf, '%c', %10.7lf, %10.7lf, %7.4lf, %7.4lf, '%c', %9.3lf, %9.3lf, %9.3f, %9.3f},\n",
 			year,month,day,mjd,ip[0],pm[0],epm[0],pm[1],epm[1],ip[1],dUT,edUT,lod,elod,ip[2],dPsi,edPsi,dEps,edEps);
 		if(i == 0) mjd0 = mjd;
 		nentry++;
